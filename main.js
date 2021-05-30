d540af8018c83a1baaf3d3593e9cf15ffc463c5b
jisu7487 <jisu2043@gmail.com>
2021-05-31 02:27:30 +0900

    db.query insert into topic

--- main.js
+++ main.js
@@ -8,11 +8,14 @@ var sanitizeHtml = require('sanitize-html');
 var mysql = require('mysql');
 var db = mysql.createConnection({
   host:'localhost',
-  user:'root',
-  password:'111111',
-  database:'opentutorials'
+  user:'GG',
+  password:'7487',
+  database:'opentutorials',
+  port : '3307'
 });
 db.connect();
+ 
+ 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
@@ -31,60 +34,37 @@ var app = http.createServer(function(request,response){
           response.end(html);
         });
       } else {
-        /*
-        fs.readdir('./data', function(error, filelist){
-          var filteredId = path.parse(queryData.id).base;
-          fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-              allowedTags:['h1']
-            });
-            var list = template.list(filelist);
-            var html = template.HTML(sanitizedTitle, list,
-              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-              ` <a href="/create">create</a>
-                <a href="/update?id=${sanitizedTitle}">update</a>
+        db.query(`SELECT * FROM topic`, function(error,topics){
+         if(error){
+           throw error;
+         }
+         db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+           if(error2){
+             throw error2;
+           }
+          var title = topic[0].title;
+          var description = topic[0].description;
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            ` <a href="/create">create</a>
+                <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="hidden" name="id" value="${queryData.id}">
                   <input type="submit" value="delete">
                 </form>`
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
-        */
-        db.query(`SELECT * FROM topic`, function(error,topics){
-          if(error){
-            throw error;
-          }
-          db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
-            if(error2){
-              throw error2;
-            }
-           var title = topic[0].title;
-           var description = topic[0].description;
-           var list = template.list(topics);
-           var html = template.HTML(title, list,
-             `<h2>${title}</h2>${description}`,
-             ` <a href="/create">create</a>
-                 <a href="/update?id=${queryData.id}">update</a>
-                 <form action="delete_process" method="post">
-                   <input type="hidden" name="id" value="${queryData.id}">
-                   <input type="submit" value="delete">
-                 </form>`
-           );
-           response.writeHead(200);
-           response.end(html);
-          })
-       });
+          );
+          response.writeHead(200);
+          response.end(html);
+         })
+      });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
+      db.query(`SELECT * FROM topic`, function(error,topics){
+        var title = 'Create';
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -94,7 +74,9 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+          `,
+          `<a href="/create">create</a>`
+        );
         response.writeHead(200);
         response.end(html);
       });
@@ -105,12 +87,18 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
+          db.query(`
+            INSERT INTO topic (title, description, created, author_id) 
+              VALUES(?, ?, NOW(), ?)`,
+            [post.title, post.description, 1], 
+            function(error, result){
+              if(error){
+                throw error;
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+            }
+          )
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
--- upload.bat
+++ upload.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "테이블 상세읽기1 ,한글깨짐 (f1 -> encoding ->reopen with encoding -> korean"
+git commit -m "db.query insert into topic"
 git push -u origin main 
